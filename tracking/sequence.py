b1065706ce1c7fa3679b5dadacf47a6db1057bb8
fmpaezri <fmpaezri@unal.edu.co>
2015-07-21 15:29:07 -0500

    View method supports videos

--- tracking/sequence.py
+++ tracking/sequence.py
@@ -1,30 +1,35 @@
 import os
 import benchmarkUtils as benchutils
 import time
+import numpy
+    
+DIR_SOURCE = 0
+VIDEO_SOURCE = 1
 
 class Sequence(object):
-    
-    def __init__(self):
+
+    def __init__(self, gtPath=None):
         super(Sequence, self).__init__()
         self.frames = []
         self.boxes = []
+        if gtPath is not None and os.path.exists(gtPath):
+            self.boxes = benchutils.parse_gt(gtPath)
         self.path = None
         self.source = None
 
-    def __init__(gtPath, self):
-        super(Sequence, self).__init__()
-        self.boxes = benchutils.parse_gt(gtPath)
-
     def validate_lenghts(self):
+        if len(self.boxes) == 0:
+            print 'Empty boxes, not validating'
+            return
         if not len(self.frames) == len(self.boxes):
             raise Exception('Number of frames ({}) and boxes ({}) do not match'.format(len(self.frames), len(self.boxes)))
 
 def fromdir(dirPath, gtPath, suffix='.jpg'):
-    aSequence = Sequence()
+    aSequence = Sequence(gtPath)
     aSequence.frames = sorted([framePath.replace(suffix, '') for framePath in os.listdir(dirPath) if framePath.endswith(suffix)])
     aSequence.path = dirPath
-    aSequence.boxes = benchutils.parse_gt(gtPath)
     aSequence.validate_lenghts()
+    aSequence.source = DIR_SOURCE
     return aSequence
   
 try:
@@ -34,25 +39,42 @@ try:
     def fromvideo(videoPath, gtPath):
         aSequence = Sequence(gtPath)
         aSequence.path = videoPath
-        aVideo = cv2.VideoCapture(aSequence.path)
-        if not aVideo.isOpened():
+        video = cv2.VideoCapture(aSequence.path)
+        if not video.isOpened():
             raise Exception('Unable to open video {}'.format(aSequence.path))
         #0 based indexing
-        aSequence.frames = map(str, range(int(aVideo.get(cv2.CAP_PROP_FRAME_COUNT))))
-        aSequence.boxes = benchutils.parse_gt(gtPath)
+        aSequence.frames = map(str, range(int(video.get(cv2.CAP_PROP_FRAME_COUNT))))
         aSequence.validate_lenghts()
+        aSequence.source = VIDEO_SOURCE
+        video.release()
         return aSequence
 
     def view(aSequence, winname='view', suffix='.jpg', fps=30):
+        if aSequence.source == VIDEO_SOURCE:
+            video = cv2.VideoCapture(aSequence.path)
+            if not video.isOpened():
+                raise Exception('Unable to open video {}'.format(aSequence.path))
+            frameIndex = 0
+            videoFps = video.get(cv2.CAP_PROP_FPS)
+            if not numpy.isnan(videoFps):
+                fps = videoFps
         cv2.namedWindow(winname)
         period = 1000.0 / fps
         for aFrame in aSequence.frames:
             start = time.time()
-            framePath = os.path.join(aSequence.path, aFrame+suffix)
-            frame = cv2.imread(framePath)
+            if aSequence.source == DIR_SOURCE:
+                framePath = os.path.join(aSequence.path, aFrame+suffix)
+                frame = cv2.imread(framePath)
+            else:
+                success, frame = video.read()
+                if not success:
+                    raise Exception('Error reading frame {}'.format(frameIndex))
+                frameIndex += 1
             cv2.imshow(winname, frame)
             end = time.time()
             cv2.waitKey(int(period-(end - start)*1000))
+        if aSequence.source == VIDEO_SOURCE:
+            video.release()
         cv2.destroyWindow(winname)
         cv2.waitKey(1)
 
