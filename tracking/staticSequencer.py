f141c93e705219be6b241425afd9b05884a06127
fmpaezri <fmpaezri@unal.edu.co>
2015-09-07 08:51:25 -0500

    Import corrections

--- tracking/staticSequencer.py
+++ tracking/staticSequencer.py
@@ -5,6 +5,8 @@ import PIL.ImageDraw
 import cv2
 import numpy
 import pycocotools.coco
+import matplotlib
+import matplotlib.pyplot
 
 if __name__ == '__main__':
     #Define paths
@@ -21,9 +23,9 @@ if __name__ == '__main__':
     print 'Number of categories {} and corresponding images {}'.format(len(catIds), len(imgIds))
 
     #Select and show an annotated image
-    img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
+    img = coco.loadImgs(imgIds[numpy.random.randint(0,len(imgIds))])[0]
     image = PIL.Image.open('%s/%s/%s'%(dataDir,dataType,img['file_name']))
-    imshow(numpy.asarray(image))
+    matplotlib.pyplot.imshow(numpy.asarray(image))
     #Load the annotations for the sampled image
     annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
     anns = coco.loadAnns(annIds)
@@ -36,9 +38,9 @@ if __name__ == '__main__':
 
     for index in xrange(120):
         aSampler.sample()
-        print 'Current sampler parameters {}'.format(aSampler)
+        #print 'Current sampler parameters {}'.format(aSampler)
         transformed = aSampler.applyTransform(crop)
         pasted = aSampler.pasteCrop(image, tuple(bounds[:2]), transformed)
-        #imshow(numpy.asarray(pasted))
+        #matplotlib.pyplot.imshow(numpy.asarray(pasted))
         cv2.imshow('sequence', numpy.asarray(pasted))
-        cv2.waitKey(30)
+        cv2.waitKey(1)
