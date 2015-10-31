cc611be5a968bab75bb8b03b5110eb25ed3653b0
Fredy Diaz <fhdiaz87@gmail.com>
2015-10-31 04:51:10 -0500

    Changes for VOT Integration

--- tracking/VideoSequenceData.py
+++ tracking/VideoSequenceData.py
@@ -5,122 +5,128 @@ import PIL.ImageDraw as ImageDraw
 import TrajectorySimulator as ts
 import TraxClient as tc
 try:
-  import cv2
-  channels = 4
+    import cv2
+    channels = 4
 except:
-  cv2 = None
-  channels = 2
+    cv2 = None
+    channels = 2
 
 #TODO: Put this configuration in an external file or rely entirely on Coco's data
-dataDir = '/home/jccaicedo/data/tracking/simulations/'
-#dataDir = '/data1/vot-challenge/simulations/'
-scene = dataDir + 'bogota.jpg'
-obj = dataDir + 'photo.jpg'
 box = [0, 100, 0, 100]
 polygon = [50, 0, 100, 50, 50, 100, 0, 50]
 imgSize = 64
 totalFrames = 60
 cam = False
+maskMove = 6
 
 MAX_SPEED_PIXELS = 1.0
 
 def fraction(b,k):
-  w = (b[2]-b[0])*(1-k)/2.
-  h = (b[3]-b[1])*(1-k)/2.
-  return [b[0]+w,b[1]+h, b[2]-w,b[3]-h]
+    w = (b[2]-b[0])*(1-k)/2.
+    h = (b[3]-b[1])*(1-k)/2.
+    return [b[0]+w,b[1]+h, b[2]-w,b[3]-h]
+
+def makeMask(w,h,box):
+    mask = np.zeros((w,h))
+    for factor in [(1.00, 1), (0.75, -1), (0.5, 1), (0.25, -1)]:
+        b = map(int, fraction(box, factor[0]))
+        mask[b[1]:b[3], b[0]:b[2]] = factor[1] # y dimensions first (rows)
+    return mask
 
 def maskFrame(frame, flow, box):
-  if flow is not None:
-    maskedF = np.zeros( (4, frame.shape[0], frame.shape[1]) )
-    maskedF[1,:,:] = flow[...,0]/10
-    maskedF[2,:,:] = flow[...,1]/10
-  else:
-    maskedF = np.zeros( (2, frame.shape[0], frame.shape[1]) )
-  maskedF[0,:,:] = (frame - 128.0)/128.0
-  maskedF[-1,:,:] = 0
-  for factor in [(1.00, 1), (0.75, -1), (0.5, 1), (0.25, -1)]:
-    b = map(int, fraction(box, factor[0]))
-    maskedF[-1, b[0]:b[2], b[1]:b[3]] = factor[1]
-  #import pylab
-  #pylab.imshow(maskedF[-1,:,:])
-  #pylab.show()
-  return maskedF
+    if flow is not None:
+        maskedF = np.zeros( (4, frame.shape[0], frame.shape[1]) )
+        maskedF[1,:,:] = flow[...,0]
+        maskedF[2,:,:] = flow[...,1]
+    else:
+        maskedF = np.zeros( (2, frame.shape[0], frame.shape[1]) )
+    maskedF[0,:,:] = (frame - 128.0)/128.0
+    maskedF[-1,:,:] = makeMask(frame.shape[0], frame.shape[1], box)
+
+    return maskedF
 
 class VideoSequenceData(object):
 
-  def __init__(self):
-    self.predictedBox = [0,0,0,0]
-    self.prevBox = [0,0,0,0]
-    self.box = [0,0,0,0]
-    self.prv = None
-    self.now = None
+    def __init__(self, workingDir='/home/jccaicedo/data/tracking/simulations/debug/'):
+        self.predictedBox = [0,0,0,0]
+        self.prevBox = [0,0,0,0]
+        self.box = [0,0,0,0]
+        self.prv = None
+        self.now = None
+        self.workingDir = workingDir
 
-  def prepareSequence(self, loadSequence=None):
-    if loadSequence is None:
-      self.dataSource = ts.TrajectorySimulator(scene, obj, box, polygon, camera=cam)
-    elif loadSequence == 'TraxClient':
-      self.dataSource = TraxClientWrapper()
-    else:
-      self.dataSource = StaticDataSource(loadSequence) 
-    self.deltaW = float(imgSize)/self.dataSource.getFrame().size[0]
-    self.deltaH = float(imgSize)/self.dataSource.getFrame().size[1]
-    b = self.dataSource.getBox()
-    self.box = map(int, [b[0]*self.deltaW, b[1]*self.deltaH, b[2]*self.deltaW, b[3]*self.deltaH])
-    self.prevBox = map(lambda x:x, self.box)
-    self.predictedBox = map(lambda x:x, self.box)
-    self.transformFrame()
-    self.prev = self.now.copy()
-    self.time = 0
+    def prepareSequence(self, loadSequence=None):
+        if loadSequence is None:
+          scene = self.workingDir + 'bogota.jpg'
+          obj = self.workingDir + 'photo.jpg'
+          self.dataSource = ts.TrajectorySimulator(scene, obj, box, polygon, camera=cam)
+        elif loadSequence == 'TraxClient':
+          self.dataSource = TraxClientWrapper()
+        else:
+          self.dataSource = StaticDataSource(loadSequence) 
+        self.scaleW = float(imgSize)/self.dataSource.getFrame().size[0]
+        self.scaleH = float(imgSize)/self.dataSource.getFrame().size[1]
+        b = self.dataSource.getBox()
+        self.box = map(int, [b[0]*self.scaleW, b[1]*self.scaleH, b[2]*self.scaleW, b[3]*self.scaleH])
+        self.prevBox = self.box[:]
+        self.predictedBox = self.box[:]
+        self.transformFrame()
+        self.prev = self.now.copy()
+        self.time = 0
 
-  def nextStep(self, mode='training'):
-    self.prevBox = map(lambda x:x, self.box)
-    end = self.dataSource.nextStep()
-    if mode == 'training':
-      b = self.dataSource.getBox()
-      self.box = map(int, [b[0]*self.deltaW, b[1]*self.deltaH, b[2]*self.deltaW, b[3]*self.deltaH])
-    else:
-      b = self.dataSource.getBox()
-      tmp = map(int, [b[0]*self.deltaW, b[1]*self.deltaH, b[2]*self.deltaW, b[3]*self.deltaH])
-      print 'Predicted:',self.predictedBox, 'Real:',tmp,'Diff:',[tmp[i]-self.predictedBox[i] for i in range(len(tmp))]
-      self.box = map(lambda x:x, self.predictedBox)
-    self.time += 1
-    return end
+    def nextStep(self, mode='training'):
+        if self.time % maskMove == 0:
+          self.prevBox = self.box[:]
+        end = self.dataSource.nextStep()
+        if mode == 'training':
+          b = self.dataSource.getBox()
+          self.box = map(int, [b[0]*self.scaleW, b[1]*self.scaleH, b[2]*self.scaleW, b[3]*self.scaleH])
+        else:
+          b = self.dataSource.getBox()
+          tmp = map(int, [b[0]*self.scaleW, b[1]*self.scaleH, b[2]*self.scaleW, b[3]*self.scaleH])
+          #print 'Predicted:',self.predictedBox, 'Real:',tmp,'Diff:',[tmp[i]-self.predictedBox[i] for i in range(len(tmp))]
+          self.box = self.predictedBox[:]
+        self.time += 1
+        return end
 
-  def getFrame(self, savePath=None):
-    if savePath is not None:
-      with open(savePath + '/rects.txt','a') as rects:
-        rects.write(' '.join(map(str,self.box)) + '\n')
-      savePath += '/' + str(self.time).zfill(4) + '.jpg'
+    def getFrame(self, savePath=None):
+        if savePath is not None:
+          with open(savePath + '/rects.txt','a') as rects:
+            rects.write(' '.join(map(str,self.box)) + '\n')
+          savePath += '/' + str(self.time).zfill(4) + '.jpg'
+    
+        self.prv = self.now
+        self.transformFrame(save=savePath, box=self.prevBox)
+        if cv2 is not None:
+          flow = cv2.calcOpticalFlowFarneback(self.prv, self.now, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        else:
+          flow = None
+    
+        return maskFrame(self.now, flow, self.prevBox)
 
-    self.prv = self.now
-    self.transformFrame(save=savePath, box=self.prevBox)
-    if cv2 is not None:
-      flow = cv2.calcOpticalFlowFarneback(self.prv, self.now, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-    else:
-      flow = None
+    def getMove(self):
+        delta = [int(self.box[i]-self.prevBox[i])/MAX_SPEED_PIXELS for i in range(len(self.box))]
+        return delta
 
-    return maskFrame(self.now, flow, self.box)
+    def setMove(self, delta):
+        #print 'Delta:',delta
+        b = [self.box[i] + round(delta[i]*MAX_SPEED_PIXELS) for i in range(len(self.box))]
+        b = [max(min(x,imgSize-1),0) for x in b]
+        rescaledBox = map(int, [b[0]/self.scaleW, b[1]/self.scaleH, b[2]/self.scaleW, b[3]/self.scaleH])
+        self.dataSource.reportBox(rescaledBox)
+        self.predictedBox = b
 
-  def getMove(self):
-    delta = [int(self.box[i]-self.prevBox[i])/MAX_SPEED_PIXELS for i in range(len(self.box))]
-    return delta
-
-  def setMove(self, delta):
-    print 'Delta:',delta
-    self.predictedBox = [round(self.box[i] + delta[i]*MAX_SPEED_PIXELS) for i in range(len(self.box))]
-    self.dataSource.reportBox(self.predictedBox)
-
-  def transformFrame(self, save=None, box=None):
-    frame = self.dataSource.getFrame()
-    frame = frame.convert('L')
-    frame = frame.resize((imgSize,imgSize),Image.ANTIALIAS)
-    '''if box is not None:
-      draw = ImageDraw.Draw(frame)
-      for f in [1,0.75,0.5,0.25]:
-        draw.rectangle(fraction(box,f),outline=255)'''
-    if save is not None:
-      frame.save(save)
-    self.now = np.array(frame)
+    def transformFrame(self, save=None, box=None):
+        frame = self.dataSource.getFrame()
+        frame = frame.convert('L')
+        frame = frame.resize((imgSize,imgSize),Image.ANTIALIAS)
+        '''if box is not None:
+          draw = ImageDraw.Draw(frame)
+          for f in [1,0.75,0.5,0.25]:
+            draw.rectangle(fraction(box,f),outline=255)'''
+        if save is not None:
+          frame.save(save)
+        self.now = np.array(frame)
 
 class StaticDataSource(object):
 
@@ -151,27 +157,43 @@ class StaticDataSource(object):
       return False
 
 class TraxClientWrapper(object):
-
+  region = None
+  
   def __init__(self):
     self.client = tc.TraxClient()
-    self.path = self.client.nextFramePath()
-    self.box = self.client.initialize()
-    # TODO: Transform box from 4 coordinates to 2 coordinates
+    self.region = self.client.getInitialRegion()
+    self.path = self.client.nextFrame()
 
   def getFrame(self):
     img = Image.open(self.path)
     return img
 
   def getBox(self):
-    return self.box
+    box = self.region.toList()
+        
+    return [box[i] for i in [0, 1, 4, 5]]
 
   def reportBox(self, box):
-    # TODO: Transform from 2 coordinates to 4 coordinates
     self.box = box
-    self.client.reportRegion(box)
+    leftTopX = box[0]
+    leftTopY = box[1]
+    width = box[2] - box[0]
+    height = box[3] - box[1]
+    rightTopX = leftTopX + width
+    rightTopY = leftTopY
+    rightBottomX = rightTopX
+    rightBottomY = rightTopY + height
+    leftBottomX = leftTopX
+    leftBottomY = leftTopY + height
+            
+    coords = [leftTopX, leftTopY, rightTopX, rightTopY, rightBottomX, rightBottomY, leftBottomX, leftBottomY]
+    boxT = tc.Box()
+    boxT.setRegion(coords)
+        
+    self.client.reportRegion(boxT)
 
   def nextStep(self):
-    self.path = self.client.nextFramePath()
+    self.path = self.client.nextFrame()
     if self.path == '':
       self.client.quit()
       return False
