25504516a31918658252700aef1fc81d53405a65
fmpaezri <fmpaezri@unal.edu.co>
2016-02-03 12:32:35 -0500

    Applied a similar structure as cnn

--- tracking/recurrent_base.py
+++ tracking/recurrent_base.py
@@ -1,3 +1,5 @@
+import argparse as ap
+
 import theano as T
 import theano.tensor as TT
 import theano.tensor.nnet as NN
@@ -8,6 +10,12 @@ import numpy.random as RNG
 import GaussianGenerator as GG
 
 from collections import OrderedDict
+
+import cPickle
+import time
+
+def clock(m, st): 
+  print m,(time.time()-st)
 
 #####################################################################
 # Usage:                                                            #
@@ -58,149 +66,6 @@ def tensor5(name=None, dtype=None):
 		dtype = T.config.floatX
 	return TT.TensorType(dtype, [False] * 5, name=name)()
 
-conv2d = NN.conv2d
-
-### Utility functions end
-
-### CONFIGURATION BEGIN
-dataDir = '/home/jccaicedo/data/simulations'
-batch_size = 32
-conv1_nr_filters = 32
-conv1_filter_row = 10
-conv1_filter_col = 10
-conv1_stride = 5
-img_row = 100
-img_col = 100
-# attentions are unused yet
-attention_row = 25
-attention_col = 25
-gru_dim = 256
-seq_len = 200
-model_name = 'model.pkl'
-zero_tail_fc = False
-variadic_length = False
-test = False
-acc_scale = 0
-zoom_scale = 0
-double_mnist = False
-dataset_name = "train"
-clutter_move = 0.5
-with_clutters = 1
-nr_objs = 2
-### CONFIGURATION END
-
-### getopt begin
-from getopt import *
-import sys
-
-try:
-	opts, args = getopt(sys.argv[1:], "", ["batch_size=", "conv1_nr_filters=", "conv1_filter_size=", "conv1_stride=", "img_size=", "gru_dim=", "seq_len=", "use_cudnn", "zero_tail_fc", "var_len", "test", "acc_scale=",
-		"zoom_scale=", "dataset=", "double_mnist", "clutter_move=", "nr_objs="])
-	for opt in opts:
-		if opt[0] == "--batch_size":
-			batch_size = int(opt[1])
-		elif opt[0] == "--conv1_nr_filters":
-			conv1_nr_filters = int(opt[1])
-		elif opt[0] == "--conv1_filter_size":
-			conv1_filter_row = conv1_filter_col = int(opt[1])
-		elif opt[0] == "--conv1_stride":
-			conv1_stride = int(opt[1])
-		elif opt[0] == "--img_size":
-			img_row = img_col = int(opt[1])
-		elif opt[0] == "--gru_dim":
-			gru_dim = int(opt[1])
-		elif opt[0] == "--seq_len":
-			seq_len = int(opt[1])
-		elif opt[0] == "--use_cudnn":
-			if T.config.device[:3] == 'gpu':
-				import theano.sandbox.cuda.dnn as CUDNN
-				if CUDNN.dnn_available():
-					print 'Using CUDNN instead of Theano conv2d'
-					conv2d = CUDNN.dnn_conv
-		elif opt[0] == "--zero_tail_fc":
-			zero_tail_fc = True
-		elif opt[0] == "--var_len":
-			variadic_length = True
-		elif opt[0] == "--test":
-			test = True
-		elif opt[0] == "--acc_scale":
-			acc_scale = float(opt[1])
-		elif opt[0] == "--zoom_scale":
-			zoom_scale = float(opt[1])
-		elif opt[0] == "--double_mnist":
-			double_mnist = True
-		elif opt[0] == "--dataset":
-			dataset_name = opt[1]
-		elif opt[0] == "--clutter_move":
-			clutter_move = float(opt[1])
-		elif opt[0] == "--nr_objs":
-			nr_objs = int(opt[1])
-	if len(args) > 0:
-		model_name = args[0]
-except:
-	pass
-### getopt end
-
-### Computed hyperparameters begin
-conv1_output_dim = ((img_row - conv1_filter_row) / conv1_stride + 1) * \
-		((img_col - conv1_filter_col) / conv1_stride + 1) * \
-		conv1_nr_filters
-gru_input_dim = conv1_output_dim + 4
-### Computed hyperparameters end
-
-print 'Initializing parameters'
-
-### NETWORK PARAMETERS BEGIN
-conv1_filters = T.shared(glorot_uniform((conv1_nr_filters, 1, conv1_filter_row, conv1_filter_col)), name='conv1_filters')
-Wr = T.shared(glorot_uniform((gru_input_dim, gru_dim)), name='Wr')
-Ur = T.shared(orthogonal((gru_dim, gru_dim)), name='Ur')
-br = T.shared(NP.zeros((gru_dim,), dtype=T.config.floatX), name='br')
-Wz = T.shared(glorot_uniform((gru_input_dim, gru_dim)), name='Wz')
-Uz = T.shared(orthogonal((gru_dim, gru_dim)), name='Uz')
-bz = T.shared(NP.zeros((gru_dim,), dtype=T.config.floatX), name='bz')
-Wg = T.shared(glorot_uniform((gru_input_dim, gru_dim)), name='Wg')
-Ug = T.shared(orthogonal((gru_dim, gru_dim)), name='Ug')
-bg = T.shared(NP.zeros((gru_dim,), dtype=T.config.floatX), name='bg')
-W_fc2 = T.shared(glorot_uniform((gru_dim, 4)) if not zero_tail_fc else NP.zeros((gru_dim, 4), dtype=T.config.floatX), name='W_fc2')
-b_fc2 = T.shared(NP.zeros((4,), dtype=T.config.floatX), name='b_fc2')
-### NETWORK PARAMETERS END
-
-print 'Building network'
-
-### Recurrent step
-# img: of shape (batch_size, nr_channels, img_rows, img_cols)
-def _step(img, prev_bbox, state):
-	# of (batch_size, nr_filters, some_rows, some_cols)
-	conv1 = conv2d(img, conv1_filters, subsample=(conv1_stride, conv1_stride))
-	act1 = TT.tanh(conv1)
-	flat1 = TT.reshape(act1, (batch_size, conv1_output_dim))
-	gru_in = TT.concatenate([flat1, prev_bbox], axis=1)
-	gru_z = NN.sigmoid(TT.dot(gru_in, Wz) + TT.dot(state, Uz) + bz)
-	gru_r = NN.sigmoid(TT.dot(gru_in, Wr) + TT.dot(state, Ur) + br)
-	gru_h_ = TT.tanh(TT.dot(gru_in, Wg) + TT.dot(gru_r * state, Ug) + bg)
-	gru_h = (1-gru_z) * state + gru_z * gru_h_
-	bbox = TT.tanh(TT.dot(gru_h, W_fc2) + b_fc2)
-	return bbox, gru_h
-
-# imgs: of shape (batch_size, seq_len, nr_channels, img_rows, img_cols)
-imgs = tensor5()
-starts = TT.matrix()
-
-# Move the time axis to the top
-_imgs = imgs.dimshuffle(1, 0, 2, 3, 4)
-sc, _ = T.scan(_step, sequences=[imgs.dimshuffle(1, 0, 2, 3, 4)], outputs_info=[starts, TT.zeros((batch_size, gru_dim))])
-
-bbox_seq = sc[0].dimshuffle(1, 0, 2)
-
-# targets: of shape (batch_size, seq_len, 4)
-targets = TT.tensor3()
-seq_len_scalar = TT.scalar()
-
-cost = ((targets - bbox_seq) ** 2).sum() / batch_size / seq_len_scalar
-
-print 'Building optimizer'
-
-params = [conv1_filters, Wr, Ur, br, Wz, Uz, bz, Wg, Ug, bg, W_fc2, b_fc2]
 ### RMSProp begin
 def rmsprop(cost, params, lr=0.0005, rho=0.9, epsilon=1e-6):
 	'''
@@ -216,84 +81,189 @@ def rmsprop(cost, params, lr=0.0005, rho=0.9, epsilon=1e-6):
 		updates[p] = new_p
 
 	return updates
-
 ### RMSprop end
 
-train = T.function([seq_len_scalar, imgs, starts, targets], [cost, bbox_seq], updates=rmsprop(cost, params) if not test else None, allow_input_downcast=True)
-tester = T.function([seq_len_scalar, imgs, starts, targets], [cost, bbox_seq], allow_input_downcast=True)
+def dump_params(model_name, params):
+    f = open(model_name, "wb")
+    cPickle.dump(map(lambda x: x.get_value(), params), f)
+    f.close()
 
-import cPickle
-import time
-def clock(m, st): 
-  print m,(time.time()-st)
+def build(batch_size, gru_input_dim, gru_dim, conv_output_dim, conv_nr_filters, conv_filter_row, conv_filter_col, conv_stride, zero_tail_fc, test, use_cudnn):
+    print 'Building network'
 
-try:
-    f = open(model_name, "rb")
-    param_saved = cPickle.load(f)
-    for _p, p in zip(params, param_saved):
-        _p.set_value(p)
-except IOError:
-    pass
+    # imgs: of shape (batch_size, seq_len, nr_channels, img_rows, img_cols)
+    imgs = tensor5()
+    starts = TT.matrix()
 
-print 'Generating dataset'
+    conv2d = NN.conv2d
+    if use_cudnn and T.config.device[:3] == 'gpu':
+        import theano.sandbox.cuda.dnn as CUDNN
+        if CUDNN.dnn_available():
+            print 'Using CUDNN instead of Theano conv2d'
+            conv2d = CUDNN.dnn_conv
 
-generator = GG.GaussianGenerator(dataDir=dataDir)
-print 'START'
+    conv_filters, Wr, Ur, br, Wz, Uz, bz, Wg, Ug, bg, W_fc2, b_fc2 = init_params(conv_nr_filters, conv_filter_row, conv_filter_col, gru_input_dim, gru_dim, zero_tail_fc)
+    params = [conv_filters, Wr, Ur, br, Wz, Uz, bz, Wg, Ug, bg, W_fc2, b_fc2]
 
-try:
-	for i in range(0, 50):
-                train_cost = test_cost = 0
-		for j in range(0, 2000):
-                        st = time.time()
-			data, label = generator.getBatchInParallel(batch_size)
-                        clock('Simulations',st)
+    ### Recurrent step
+    # img: of shape (batch_size, nr_channels, img_rows, img_cols)
+    def _step(img, prev_bbox, state):
+        # of (batch_size, nr_filters, some_rows, some_cols)
+        conv1 = conv2d(img, conv_filters, subsample=(conv_stride, conv_stride))
+        act1 = TT.tanh(conv1)
+        flat1 = TT.reshape(act1, (batch_size, conv_output_dim))
+        gru_in = TT.concatenate([flat1, prev_bbox], axis=1)
+        gru_z = NN.sigmoid(TT.dot(gru_in, Wz) + TT.dot(state, Uz) + bz)
+        gru_r = NN.sigmoid(TT.dot(gru_in, Wr) + TT.dot(state, Ur) + br)
+        gru_h_ = TT.tanh(TT.dot(gru_in, Wg) + TT.dot(gru_r * state, Ug) + bg)
+        gru_h = (1-gru_z) * state + gru_z * gru_h_
+        bbox = TT.tanh(TT.dot(gru_h, W_fc2) + b_fc2)
+        return bbox, gru_h
 
-                        st = time.time()
-			data = data[:, :, NP.newaxis, :, :] / 255.0
-			label = label / (img_row / 2.) - 1.
-                        clock('Normalization',st)
+    # Move the time axis to the top
+    sc, _ = T.scan(_step, sequences=[imgs.dimshuffle(1, 0, 2, 3, 4)], outputs_info=[starts, TT.zeros((batch_size, gru_dim))])
 
-                        # We can also implement a 'replay memory' here to store previous simulations and reuse them again later during training.
-                        # The basic idea is to store sequences in a tensor of a predefined capacity, and when it's full we start sampling sequences
-                        # from the memory with certain probability. The rest of the time new sequences are simulated. This could save some processing time.
+    bbox_seq = sc[0].dimshuffle(1, 0, 2)
 
-                        st = time.time()
-			cost, bbox_seq = train(seq_len, data, label[:, 0, :], label)
-                        clock('Training',st)
-			'''left = NP.max([bbox_seq[:, :, 0], label[:, :, 0]], axis=0)
-			top = NP.max([bbox_seq[:, :, 1], label[:, :, 1]], axis=0)
-			right = NP.min([bbox_seq[:, :, 2], label[:, :, 2]], axis=0)
-			bottom = NP.min([bbox_seq[:, :, 3], label[:, :, 3]], axis=0)
-			intersect = (right - left) * ((right - left) > 0) * (bottom - top) * ((bottom - top) > 0)
-			label_area = (label[:, :, 2] - label[:, :, 0]) * (label[:, :, 2] - label[:, :, 0] > 0) * (label[:, :, 3] - label[:, :, 1]) * (label[:, :, 3] - label[:, :, 1] > 0)
-			predict_area = (bbox_seq[:, :, 2] - bbox_seq[:, :, 0]) * (bbox_seq[:, :, 2] - bbox_seq[:, :, 0] > 0) * (bbox_seq[:, :, 3] - bbox_seq[:, :, 1]) * (bbox_seq[:, :, 3] - bbox_seq[:, :, 1] > 0)
-			union = label_area + predict_area - intersect'''
-			print i, j, cost
-                        train_cost += cost
-			'''data, label = generator.getBatch(batch_size)
-			data = data[:, :, NP.newaxis, :, :] / 255.0
-			label = label / (img_row / 2.) - 1.
-			cost, bbox_seq = tester(seq_len, data, label[:, 0, :], label)
-			left = NP.max([bbox_seq[:, :, 0], label[:, :, 0]], axis=0)
-			top = NP.max([bbox_seq[:, :, 1], label[:, :, 1]], axis=0)
-			right = NP.min([bbox_seq[:, :, 2], label[:, :, 2]], axis=0)
-			bottom = NP.min([bbox_seq[:, :, 3], label[:, :, 3]], axis=0)
-			intersect = (right - left) * ((right - left) > 0) * (bottom - top) * ((bottom - top) > 0)
-			label_area = (label[:, :, 2] - label[:, :, 0]) * (label[:, :, 2] - label[:, :, 0] > 0) * (label[:, :, 3] - label[:, :, 1]) * (label[:, :, 3] - label[:, :, 1] > 0)
-			predict_area = (bbox_seq[:, :, 2] - bbox_seq[:, :, 0]) * (bbox_seq[:, :, 2] - bbox_seq[:, :, 0] > 0) * (bbox_seq[:, :, 3] - bbox_seq[:, :, 1]) * (bbox_seq[:, :, 3] - bbox_seq[:, :, 1] > 0)
-			union = label_area + predict_area - intersect
-                        print i, j, cost
-                        test_cost += cost
-			iou = intersect / union
-			print NP.average(iou, axis=0)       # per frame
-			print NP.average(iou, axis=1)       # per batch'''
-                print 'Epoch average loss (train, test)', train_cost / 2000, test_cost / 2000
-                f = open(model_name + str(i), "wb")
-		cPickle.dump(map(lambda x: x.get_value(), params), f)
-                f.close()
-except KeyboardInterrupt:
-	if not test:
-		print 'Saving...'
-		f = open(model_name, "wb")
-		cPickle.dump(map(lambda x: x.get_value(), params), f)
-		f.close()
+    # targets: of shape (batch_size, seq_len, 4)
+    targets = TT.tensor3()
+    seq_len_scalar = TT.scalar()
+
+    cost = ((targets - bbox_seq) ** 2).sum() / batch_size / seq_len_scalar
+
+    print 'Building optimizer'
+
+    train = T.function([seq_len_scalar, imgs, starts, targets], [cost, bbox_seq], updates=rmsprop(cost, params) if not test else None, allow_input_downcast=True)
+    tester = T.function([seq_len_scalar, imgs, starts, targets], [cost, bbox_seq], allow_input_downcast=True)
+    
+    return train, tester, params
+
+def init_params(conv_nr_filters, conv_filter_row, conv_filter_col, gru_input_dim, gru_dim, zero_tail_fc):
+    
+    print 'Initializing parameters'
+
+    ### NETWORK PARAMETERS BEGIN
+    conv_filters = T.shared(glorot_uniform((conv_nr_filters, 1, conv_filter_row, conv_filter_col)), name='conv_filters')
+    Wr = T.shared(glorot_uniform((gru_input_dim, gru_dim)), name='Wr')
+    Ur = T.shared(orthogonal((gru_dim, gru_dim)), name='Ur')
+    br = T.shared(NP.zeros((gru_dim,), dtype=T.config.floatX), name='br')
+    Wz = T.shared(glorot_uniform((gru_input_dim, gru_dim)), name='Wz')
+    Uz = T.shared(orthogonal((gru_dim, gru_dim)), name='Uz')
+    bz = T.shared(NP.zeros((gru_dim,), dtype=T.config.floatX), name='bz')
+    Wg = T.shared(glorot_uniform((gru_input_dim, gru_dim)), name='Wg')
+    Ug = T.shared(orthogonal((gru_dim, gru_dim)), name='Ug')
+    bg = T.shared(NP.zeros((gru_dim,), dtype=T.config.floatX), name='bg')
+    W_fc2 = T.shared(glorot_uniform((gru_dim, 4)) if not zero_tail_fc else NP.zeros((gru_dim, 4), dtype=T.config.floatX), name='W_fc2')
+    b_fc2 = T.shared(NP.zeros((4,), dtype=T.config.floatX), name='b_fc2')
+    ### NETWORK PARAMETERS END
+
+    return conv_filters, Wr, Ur, br, Wz, Uz, bz, Wg, Ug, bg, W_fc2, b_fc2
+
+def build_parser():
+    parser = ap.ArgumentParser(description='Trains a RNN tracker')
+    parser.add_argument('--dataDir', help='Directory of trajectory model', type=str, default='/home/fmpaezri/repos/localization-agent/notebooks')
+    parser.add_argument('--batch_size', help='Number of elements in batch', type=int, default=32)
+    parser.add_argument('--conv_nr_filters', help='Number of feature filters', type=int, default=32)
+    parser.add_argument('--conv_filter_row', help='Rows of feature filters', type=int, default=10)
+    parser.add_argument('--conv_filter_col', help='Columns of feature filters', type=int, default=10)
+    parser.add_argument('--conv_stride', help='Convolutional stride', type=int, default=5)
+    parser.add_argument('--img_row', help='Image rows', type=int, default=100)
+    parser.add_argument('--img_col', help='Image cols', type=int, default=100)
+    parser.add_argument('--gru_dim', help='Dimension of GRU state', type=int, default=256)
+    parser.add_argument('--seq_len', help='Lenght of sequences', type=int, default=60)
+    parser.add_argument('--model_name', help='Name of model file', type=str, default='model.pkl')
+    parser.add_argument('--zero_tail_fc', help='', type=bool, default=False)
+    parser.add_argument('--test', help='', type=bool, default=False)
+    parser.add_argument('--use_cudnn', help='Use CUDA CONV or THEANO', type=bool, default=False)
+    return parser
+
+### Utility functions end
+
+if __name__ == '__main__':
+    ### CONFIGURATION BEGIN
+    parser = build_parser()
+    args = parser.parse_args()
+    globals().update(vars(args))
+    ### CONFIGURATION END
+
+    ### Computed hyperparameters begin
+    conv_output_dim = ((img_row - conv_filter_row) / conv_stride + 1) * \
+        ((img_col - conv_filter_col) / conv_stride + 1) * \
+        conv_nr_filters
+    gru_input_dim = conv_output_dim + 4
+    ### Computed hyperparameters end
+
+    train, tester, params = build(batch_size, gru_input_dim, gru_dim, conv_output_dim, conv_nr_filters, conv_filter_row, conv_filter_col, conv_stride, zero_tail_fc, test, use_cudnn)
+
+    try:
+        f = open(model_name, "rb")
+        param_saved = cPickle.load(f)
+        for _p, p in zip(params, param_saved):
+            _p.set_value(p)
+    except IOError:
+        pass
+
+    print 'Generating dataset'
+
+    generator = GG.GaussianGenerator(dataDir=dataDir, seqLength=seq_len, imageSize=img_row, grayscale=True)
+    print 'START'
+
+    try:
+        for i in range(0, 50):
+            train_cost = test_cost = 0
+            for j in range(0, 2000):
+                st = time.time()
+                data, label = generator.getBatchInParallel(batch_size)
+                clock('Simulations',st)
+
+                st = time.time()
+                print 'Initial data shape:', data.shape
+                if generator.grayscale:
+                    data = data[:, :, NP.newaxis, :, :]
+                else:
+                    data = data.transpose((0,1,4,2,3))
+                data /= 255.0
+                print 'Final data shape:', data.shape
+                label = label / (img_row / 2.) - 1.
+                clock('Normalization',st)
+
+                # We can also implement a 'replay memory' here to store previous simulations and reuse them again later during training.
+                # The basic idea is to store sequences in a tensor of a predefined capacity, and when it's full we start sampling sequences
+                # from the memory with certain probability. The rest of the time new sequences are simulated. This could save some processing time.
+
+                st = time.time()
+                cost, bbox_seq = train(seq_len, data, label[:, 0, :], label)
+                clock('Training',st)
+                '''left = NP.max([bbox_seq[:, :, 0], label[:, :, 0]], axis=0)
+                top = NP.max([bbox_seq[:, :, 1], label[:, :, 1]], axis=0)
+                right = NP.min([bbox_seq[:, :, 2], label[:, :, 2]], axis=0)
+                bottom = NP.min([bbox_seq[:, :, 3], label[:, :, 3]], axis=0)
+                intersect = (right - left) * ((right - left) > 0) * (bottom - top) * ((bottom - top) > 0)
+                label_area = (label[:, :, 2] - label[:, :, 0]) * (label[:, :, 2] - label[:, :, 0] > 0) * (label[:, :, 3] - label[:, :, 1]) * (label[:, :, 3] - label[:, :, 1] > 0)
+                predict_area = (bbox_seq[:, :, 2] - bbox_seq[:, :, 0]) * (bbox_seq[:, :, 2] - bbox_seq[:, :, 0] > 0) * (bbox_seq[:, :, 3] - bbox_seq[:, :, 1]) * (bbox_seq[:, :, 3] - bbox_seq[:, :, 1] > 0)
+                union = label_area + predict_area - intersect'''
+                print i, j, cost
+                train_cost += cost
+                '''data, label = generator.getBatch(batch_size)
+                data = data[:, :, NP.newaxis, :, :] / 255.0
+                label = label / (img_row / 2.) - 1.
+                cost, bbox_seq = tester(seq_len, data, label[:, 0, :], label)
+                left = NP.max([bbox_seq[:, :, 0], label[:, :, 0]], axis=0)
+                top = NP.max([bbox_seq[:, :, 1], label[:, :, 1]], axis=0)
+                right = NP.min([bbox_seq[:, :, 2], label[:, :, 2]], axis=0)
+                bottom = NP.min([bbox_seq[:, :, 3], label[:, :, 3]], axis=0)
+                intersect = (right - left) * ((right - left) > 0) * (bottom - top) * ((bottom - top) > 0)
+                label_area = (label[:, :, 2] - label[:, :, 0]) * (label[:, :, 2] - label[:, :, 0] > 0) * (label[:, :, 3] - label[:, :, 1]) * (label[:, :, 3] - label[:, :, 1] > 0)
+                predict_area = (bbox_seq[:, :, 2] - bbox_seq[:, :, 0]) * (bbox_seq[:, :, 2] - bbox_seq[:, :, 0] > 0) * (bbox_seq[:, :, 3] - bbox_seq[:, :, 1]) * (bbox_seq[:, :, 3] - bbox_seq[:, :, 1] > 0)
+                union = label_area + predict_area - intersect
+                print i, j, cost
+                test_cost += cost
+                iou = intersect / union
+                print NP.average(iou, axis=0)       # per frame
+                print NP.average(iou, axis=1)       # per batch'''
+            print 'Epoch average loss (train, test)', train_cost / 2000, test_cost / 2000
+            dump_params(model_name + str(i), params)
+    except KeyboardInterrupt:
+        if not test:
+            print 'Saving...'
+            dump_params(model_name, params)
